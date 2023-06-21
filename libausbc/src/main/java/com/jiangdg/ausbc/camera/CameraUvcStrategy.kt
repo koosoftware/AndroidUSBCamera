5684f0930bb07423f11f96534e91e7d6ca630e56
koo <koomingchin@gmail.com>
2023-06-21 21:01:53 +0800

    Fix 2 errors in crash report

--- libausbc/src/main/java/com/jiangdg/ausbc/camera/CameraUvcStrategy.kt
+++ libausbc/src/main/java/com/jiangdg/ausbc/camera/CameraUvcStrategy.kt
@@ -47,9 +47,7 @@ class CameraUvcStrategy(ctx: Context, deviceId: Int?) : ICameraStrategy(ctx) {
     private var mDevSettableFuture: SettableFuture<UsbDevice?>? = null
     private var mCtrlBlockSettableFuture: SettableFuture<USBMonitor.UsbControlBlock?>? = null
     private val mConnectSettableFuture: SettableFuture<Boolean> = SettableFuture()
-    private val mNV21DataQueue: LinkedBlockingDeque<ByteArray> by lazy {
-        LinkedBlockingDeque(MAX_NV21_DATA)
-    }
+    private var mNV21DataQueue: LinkedBlockingDeque<ByteArray>? = LinkedBlockingDeque<ByteArray>(MAX_NV21_DATA)
     private val mRequestPermission: AtomicBoolean by lazy {
         AtomicBoolean(false)
     }
@@ -247,7 +245,7 @@ class CameraUvcStrategy(ctx: Context, deviceId: Int?) : ICameraStrategy(ctx) {
             return
         }
         mSaveImageExecutor.submit {
-            val data = mNV21DataQueue.pollFirst(CAPTURE_TIMES_OUT_SEC, TimeUnit.SECONDS)
+            val data = mNV21DataQueue!!.pollFirst(CAPTURE_TIMES_OUT_SEC, TimeUnit.SECONDS)
             if (data == null || getRequest() == null) {
                 mMainHandler.post {
                     mCaptureDataCb?.onError("Times out or camera request is null")
@@ -534,6 +532,13 @@ class CameraUvcStrategy(ctx: Context, deviceId: Int?) : ICameraStrategy(ctx) {
         mUsbMonitor?.unregister()
         mUsbMonitor?.destroy()
         mUsbMonitor = null
+
+        if (mNV21DataQueue!!.size > 0) {
+            mNV21DataQueue!!.remove()
+            mNV21DataQueue!!.clear()
+            mNV21DataQueue = null
+        }
+        
         if (Utils.debugCamera) {
             Logger.i(TAG, "unRegister uvc device monitor")
         }
@@ -803,10 +808,10 @@ class CameraUvcStrategy(ctx: Context, deviceId: Int?) : ICameraStrategy(ctx) {
                 val data = ByteArray(capacity())
                 get(data)
                 cb.onPreviewData(data, IPreviewDataCallBack.DataFormat.NV21)
-                if (mNV21DataQueue.size >= MAX_NV21_DATA) {
-                    mNV21DataQueue.removeLast()
+                if (mNV21DataQueue!!.size >= MAX_NV21_DATA) {
+                    mNV21DataQueue!!.removeLast()
                 }
-                mNV21DataQueue.offerFirst(data)
+                mNV21DataQueue!!.offerFirst(data)
             }
         }
     }
